96d42758b177e1bc5dc82056fd02c5abc1c6880e
ankur <ankur.s@copperdigital.com>
2023-06-20 12:50:05 +0530

    area-management UI completed

--- src/app/area/area-routing.module.ts
+++ src/app/area/area-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AreaManagementComponent } from './area-management/area-management.component';
+import { RootComponent } from './root/root.component';
 
 const routes: Routes = [
   {
     path: 'area-management',
-    component: AreaManagementComponent
+    component: RootComponent
   },
 ];
 
--- src/app/area/area.module.ts
+++ src/app/area/area.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AreaRoutingModule } from './area-routing.module';
 import { AreaManagementComponent } from './area-management/area-management.component';
+import { RootComponent } from './root/root.component';
 
 
 @NgModule({
   declarations: [
-    AreaManagementComponent
+    AreaManagementComponent,
+    RootComponent
   ],
   imports: [
     CommonModule,
